d343bd8303e7708c7c3c2d831ebde130d8a1c72f
Park Seongguk <psk2013@naver.com>
2026-06-30 22:35:16 +0900

    Bug fix + 주석 추가

--- CH6_App_Package_Container/greeter.sh
+++ CH6_App_Package_Container/greeter.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
+# 제공된 이름을 출력하거나 아무 이름이 제공되지 않은 경우 대체 인사말 표시
 
 set -o errexit
 set -o errtrace
 set -o pipefail
 
-name = "${1}"
+name="${1}"
 
 if [ -z "$name" ]
 then
 	printf "You're awesome!\n"
 else
-	printf "Hello %s, you're awesome!\n" ${name}
+	printf "Hello %s, you're awesome!\n" "$name"
 fi
